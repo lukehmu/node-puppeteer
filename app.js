04e008d981b351be421b0fce313be50342ba9c94
Luke <luke@electricputty.co.uk>
2019-07-24 16:56:43 +0100

    change app root dir, to fix heroku issue

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const routes = require('./routes')
 
-global.appRoot = path.resolve(__dirname)
+// global.appRoot = path.resolve(__dirname)
+global.appRoot = path.dirname(require.main.filename)
 
 const app = express()
 app.use(bodyParser.json())
@@ -17,3 +18,4 @@ app.use(
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}!`))
+// console.log(path.dirname(require.main.filename))
